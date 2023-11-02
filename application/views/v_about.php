2a5fa9c2ce7c6cb894687285885b8e2ec41b1ae5
cestthelavie <faridhasan0987>
2023-11-02 20:24:27 +0700

    first init github

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama : FARID HASAN</li>
+        <li>NIM : 19220270</li>
+    </ul>
+    <br>
+    <h4>ALAMAT</h4>
+    <ul type="none">
+        <li>Jalan KEMUNING BLOK C2 NO 12</li>
+    </ul>
+    <h4>TEMPAT LAHIR</h4>
+    <ul type="none">
+        <li>TANGERANG</li>
+    </ul>
+    <h4>OLAHRAGA FAVORIT</h4>
+    <ul type="square">
+        <li>LARI</li>
+        <li>GAME</li>
+    </ul>
+    
+</section>
---
+++ application/views/v_blog.php
@@ -0,0 +1,5 @@
+<section>
+    <p>selamat datang di <br>
+        blog website web bina sarana informatika </p>
+</section>
+
